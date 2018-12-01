7ae9105d115320d1a98f28339141a22760cf4f41
Ken Miller <kennethdmiller3@gmail.com>
2018-12-01 16:29:45 -0600

    Implementation for Day 1

--- 1/1.cpp
+++ 1/1.cpp
@@ -1,17 +1,62 @@
 #include <iostream>
+#include <vector>
+#include <unordered_set>
+
+// https://adventofcode.com/2018/day/1
+
+// read a list of frequency changes from the specified input stream
+void ReadFrequencyChanges(std::vector<int> &output, std::istream &input)
+{
+	int change;
+	while (input >> change)
+	{
+		output.push_back(change);
+	}
+}
+
+// PART 1
+void Part1(std::vector<int> const &changes)
+{
+	int frequency = 0;
+	for (int change : changes)
+	{
+		frequency += change;
+	}
+	std::cout << "Part 1: final frequency " << frequency << "\n";
+}
+
+// PART 2
+void Part2(std::vector<int> const &changes)
+{
+	std::unordered_set<int> frequencies;
+
+	int frequency = 0;
+	int iteration = 0;
+	frequencies.insert(frequency);
+	for (;;)
+	{
+		for (int change : changes)
+		{
+			frequency += change;
+			++iteration;
+			if (frequencies.find(frequency) != frequencies.end())
+			{
+				// found the frequency
+				std::cout << "Part 2: repeated frequency " << frequency << " on iteration " << iteration << "\n";
+				return;
+			}
+			frequencies.insert(frequency);
+		}
+	}
+}
 
 int main()
 {
-    std::cout << "Hello World!\n"; 
+	std::vector<int> changes;
+	ReadFrequencyChanges(changes, std::cin);
+
+	Part1(changes);
+	Part2(changes);
+
+	return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
